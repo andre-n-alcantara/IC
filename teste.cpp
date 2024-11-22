e71e26592790650eb542ea632f85a6211fcfc2d2
andre-n-alcantara <andre.n.alcantara@ufv.br>
2024-11-22 10:58:42 -0300

    gerar similar à de acerto

--- teste.cpp
+++ teste.cpp
@@ -1,30 +1,35 @@
 #include <iostream>
-#include <vector>
+#include <string>
 #include <algorithm>
 
-using namespace std;
-
 int main() {
-    int N, M;
-    cin >> N >> M;
+    int area1, area2, area3;
+    std::string color1, color2, color3;
 
-    vector<int> v(M);
-    for (int i = 0; i < M; i++) {
-        cin >> v[i];
-    }
+    // Leia as áreas das paredes e as cores
+    std::cin >> area1 >> area2 >> area3;
+    std::cin >> color1 >> color2 >> color3;
 
-    sort(v.begin(), v.end());
+    // Ordene as áreas das paredes em ordem crescente
+    int areas[] = {area1, area2, area3};
+    std::sort(areas, areas + 3);
 
-    int cnt = 0;
-    int curr = 1;
-    for (int i = 0; i < M; i++) {
-        if (v[i] >= curr) {
-            cnt++;
-            curr = v[i] + 1;
-        }
-    }
+    // Associe as cores às áreas das paredes ordenadas
+    std::string colors[3];
+    if (area1 == areas[0]) colors[0] = color1;
+    if (area1 == areas[1]) colors[1] = color1;
+    if (area1 == areas[2]) colors[2] = color1;
 
-    cout << cnt << endl;
+    if (area2 == areas[0]) colors[0] = color2;
+    if (area2 == areas[1]) colors[1] = color2;
+    if (area2 == areas[2]) colors[2] = color2;
+
+    if (area3 == areas[0]) colors[0] = color3;
+    if (area3 == areas[1]) colors[1] = color3;
+    if (area3 == areas[2]) colors[2] = color3;
+
+    // Imprima a cor correspondente à parede intermediária
+    std::cout << colors[1] << std::endl;
 
     return 0;
 }
