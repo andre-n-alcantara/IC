2d265abe01338c17d1ab9b32a46f79859cbe7967
andre-n-alcantara <andre.n.alcantara@ufv.br>
2024-11-21 22:32:27 -0300

    sumbit-form funcional e primeiro teste

--- submit-form.py
+++ submit-form.py
@@ -1,25 +1,44 @@
 import requests
+import os
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select # Import Select class
+from selenium.common.exceptions import TimeoutException #Import TimeoutException
+base_url ='https://olimpiada.ic.unicamp.br/pratique/ps/2021/f1/baralho/'
+file_path = './teste.cpp'
+file_path = os.path.abspath(file_path)
+def submit_file(base_url, file_path):
+    file_path = os.path.abspath(file_path)
 
-html = urlopen('https://olimpiada.ic.unicamp.br/pratique/ps/2021/f1/baralho/')
-bs = BeautifulSoup(html, 'html.parser')
-form_elem = bs.find('form')
-form_data = {}
-#select
-select_elem = form_elem.find('select')
-selected_option = None
-for option in select_elem.find_all('option'):
-    option_value = option.get('value')
-    if 'g++' in option_value or 'C++' in option_value:
-        selected_option = option_value
-        break
-if selected_option:
-    form_data[select_elem.get('name')] = selected_option
+    # Use selenium to handle delayed redirects
+    options = webdriver.FirefoxOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Firefox(options=options)
 
-file_path = ''
-action_url = form_elem.get('action')
-with open(file_path, 'rb') as file:
-    files = {'data': file}
-response = requests.post(action_url, data=form_data, files=files)
-print(response.text)
+    driver.get(base_url)  # Navigate to the initial redirect URL
+    select_elem = Select(driver.find_element(By.TAG_NAME, 'select'))
+    for option in select_elem.options:
+        if 'g++' in option.text or 'C++' in option.text:
+            option.click()
+            break
+    file_input = driver.find_element(By.NAME, 'data')
+    file_input.send_keys(file_path)
+
+    driver.find_element(By.TAG_NAME, 'form').submit()
+    # Wait for the final redirect (adjust timeout as needed)
+    try:
+        final_url = WebDriverWait(driver, 30).until(
+            EC.url_contains('resultado')  # Wait for URL to change
+        )
+
+        # Find the <center> element and print its inner text
+        center_element = driver.find_element(By.TAG_NAME, 'center')
+        print(center_element.text)
+    except TimeoutException:
+        print("Timeout waiting for redirect.")
+
+    driver.quit()  # Close the browser
