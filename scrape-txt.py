4f0206f9f26e9373da323af1117b4dc813530534
andre-n-alcantara <andre.n.alcantara@ufv.br>
2024-11-21 21:29:41 -0300

    rotular txt

--- scrape-txt.py
+++ scrape-txt.py
@@ -13,19 +13,23 @@ def get_text(base_url, href):
   edicao = re.search(pattern, href).group()
   edicao = edicao[1:len(edicao)-1]
 
+  pattern = r"/f[0-9]/"
+  fase = re.search(pattern, href).group()
+  fase = fase[1:len(fase)-1]
+  
   bs = BeautifulSoup(html, 'html.parser')
   form_element = bs.find('form')
   parent_element = form_element.find_parent()
   form_element.decompose()
+  
+  return file_name,edicao,fase,parent_element.text
 
-  return file_name,edicao,parent_element.text
-
-def save_txt(root, file_name, edicao):
+def save_txt(root, file_name, edicao, fase, nivel, text):
   path = os.path.join(root,'OBI'+edicao,file_name)
   if not os.path.exists(path):
     print(f'Erro! {file_name} nao existe.')
   os.makedirs(path, exist_ok=True)
-  path = os.path.join(path, file_name+'.txt')
+  path = os.path.join(path, nivel+'-'+fase+'-'+file_name+'.txt')
   with open(path,'w',encoding='utf-8') as f:
     f.write(text)     
 
@@ -42,8 +46,8 @@ for nivel in niveis:
   base_url="https://olimpiada.ic.unicamp.br"
   for href in hrefs:
     print(href)
-    file_name,edicao,text = get_text(base_url,href)
-    save_txt(root, file_name, edicao)
+    file_name,edicao,fase,text = get_text(base_url,href,nivel)
+    save_txt(root, file_name, edicao, fase, nivel, text)
 
 
 
