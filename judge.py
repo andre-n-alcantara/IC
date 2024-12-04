a30f3cd7b3623e4e034ec97a4e817cae6dfc6086
andre-n-alcantara <andre.n.alcantara@ufv.br>
2024-12-04 15:56:44 -0300

    judge.py

---
+++ judge.py
@@ -0,0 +1,50 @@
+import os, re
+root = './sol'
+list_pass= [] 
+list_CE= []
+list_EE= []
+list_TLE= []
+list_MLE= []
+score = {}
+errors = {}
+for file in os.listdir(root):
+    if file.endswith('.txt') and file[:-4] not in score:
+        with open(os.path.join(root, file), 'r') as f:
+            content = f.read()
+            file = file[:-4]
+            error = content.split('Saída de erro padrão (stderr):')[-1].rstrip()
+            if len(error)>0:
+                errors[file] = error
+            search = re.search(r'Pontuação \d+ / 100', content)
+            score[file] = int(search.group().split()[-3])
+            if 'Compilação terminou com sucesso' not in content:
+                list_CE.append(file)
+                continue
+            search = re.search(r'Limite de tempo permitido .+ s', content)
+            time_lim = float(search.group().split()[-2].replace(',','.'))
+            search = re.search(r'Máximo tempo usado .+ s', content)
+            time_exe = float(search.group().split()[-2].replace(',','.'))
+            if time_exe >= time_lim:
+                list_TLE.append(file)
+                continue
+            search = re.search(r'Limite de memória permitido .+ .B', content)
+            mem_lim,unit = float(search.group().split()[-2:])
+            if unit == 'MB':
+                mem_lim *= 1000
+            elif unit == 'GB':
+                mem_lim *= 1000000
+            search = re.search(r'Máxima memória usada .+ .B', content)
+            mem_exe,unit = float(search.group().split()[-2:])
+            if unit == 'MB':
+                mem_lim *= 1000
+            elif unit == 'GB':
+                mem_lim *= 1000000
+            if mem_exe >= mem_lim:
+                list_MLE.append(file)
+                continue
+            if len(error)>0:
+                list_EE.append(file)
+            else:
+                list_pass.append(file)
+            
+
--- teste.cpp
+++ teste.cpp
@@ -1,36 +1,54 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+int L, C;
+vector<vector<int>> chairs;
+
+void print_solution(vector<pair<char, pair<int, int>>> solution) {
+    cout << solution.size() << endl;
+    for (auto& move : solution) {
+        cout << move.first << " " << move.second.first << " " << move.second.second << endl;
+    }
+}
+
 int main() {
-    int N, XC, YC, R;
-    cin >> N >> XC >> YC >> R;
-
-    int hits = 0;
-    for (int i = 0; i < N; i++) {
-        int X1, Y1, X2, Y2;
-        cin >> X1 >> Y1 >> X2 >> Y2;
-
-        double dx = X2 - X1;
-        double dy = Y2 - Y1;
-        double a = dx * dx + dy * dy;
-        double b = 2 * (dx * (X1 - XC) + dy * (Y1 - YC));
-        double c = (X1 - XC) * (X1 - XC) + (Y1 - YC) * (Y1 - YC) - R * R;
-
-        double det = b * b - 4 * a * c;
-        if (det >= 0) {
-            det = sqrt(det);
-            double t1 = (-b - det) / (2 * a);
-            double t2 = (-b + det) / (2 * a);
-
-            if ((t1 >= 0 && t1 <= 1) || (t2 >= 0 && t2 <= 1)) {
-                hits++;
-            }
+    cin >> L >> C;
+    chairs.resize(L, vector<int>(C));
+    for (int i = 0; i < L; ++i) {
+        for (int j = 0; j < C; ++j) {
+            cin >> chairs[i][j];
         }
     }
 
-    cout << hits << endl;
+    vector<pair<char, pair<int, int>>> solution;
+
+    // Resolve as linhas
+    for (int i = 0; i < L; ++i) {
+        if (!is_sorted(chairs[i].begin(), chairs[i].end())) {
+            sort(chairs[i].begin(), chairs[i].end());
+            solution.emplace_back('L', i + 1, i + 2);
+        }
+    }
+
+    // Resolve as colunas
+    for (int j = 0; j < C; ++j) {
+        vector<int> col(L);
+        for (int i = 0; i < L; ++i) {
+            col[i] = chairs[i][j];
+        }
+        if (!is_sorted(col.begin(), col.end())) {
+            sort(col.begin(), col.end());
+            for (int i = 0; i < L; ++i) {
+                chairs[i][j] = col[i];
+            }
+            solution.emplace_back('C', j + 1, j + 2);
+        }
+    }
+
+    print_solution(solution);
 
     return 0;
 }
