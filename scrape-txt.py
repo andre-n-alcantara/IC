09fc0661e0f58deafd1cfcab9f44d35ebf51c33f
andre-n-alcantara <andre.n.alcantara@ufv.br>
2025-03-18 08:33:26 -0300

    comentando

--- scrape-txt.py
+++ scrape-txt.py
@@ -6,80 +6,80 @@ import os
 import requests
 
 def get_text(base_url, href):
-  html = urlopen(base_url+href)
-  pattern = r"[^/]+/$"
-  file_name = re.search(pattern, href).group()
-  file_name = file_name[:len(file_name)-1]
+    html = urlopen(base_url+href)
+    pattern = r"[^/]+/$" # EXTRAI NOME DA QUESTAO PELO TEXTO DA URL
+    file_name = re.search(pattern, href).group()
+    file_name = file_name[:len(file_name)-1]
 
-  pattern = r"/[0-9]+/"
-  edicao = re.search(pattern, href).group()
-  edicao = edicao[1:len(edicao)-1]
+    pattern = r"/[0-9]+/" # EXTRAI EDICAO DA QUESTAO PELO TEXTO DA URL
+    edicao = re.search(pattern, href).group()
+    edicao = edicao[1:len(edicao)-1]
 
-  pattern = r"/f[0-9]/"
-  fase = re.search(pattern, href).group()
-  fase = fase[1:len(fase)-1]
-  
-  bs = BeautifulSoup(html, 'html.parser')
-  form_element = bs.find('form')
-  parent_element = form_element.find_parent()
-  form_element.decompose()
-  
-  return file_name,edicao,fase,parent_element.text
+    pattern = r"/f[0-9]/" # EXTRAI FASE DA QUESTAO PELO TEXTO DA URL
+    fase = re.search(pattern, href).group()
+    fase = fase[1:len(fase)-1]
+    
+    bs = BeautifulSoup(html, 'html.parser')
+    form_element = bs.find('form')
+    parent_element = form_element.find_parent() # ENUNCIADO DA QUESTAO ESTA ACIMA DO FORM (DESCOBERTO POR INSPECAO DA PAGINA)
+    form_element.decompose()
+    
+    return file_name,edicao,fase,parent_element.text
 
 def save_txt(root, file_name, edicao, fase, nivel, text):
-  path = os.path.join(root,'OBI'+edicao,file_name)
-  if not os.path.exists(path):
-    print(f'Erro! {file_name} nao existe.')
-  os.makedirs(path, exist_ok=True)
-  path = os.path.join(path, nivel+'-'+fase+'-'+file_name+'.txt')
-  with open(path,'w',encoding='utf-8') as f:
-    f.write(text)
+    path = os.path.join(root,'OBI'+edicao,file_name)
+    if not os.path.exists(path):
+        print(f'Erro! {file_name} nao existe.')
+    os.makedirs(path, exist_ok=True)
+    path = os.path.join(path, nivel+'-'+fase+'-'+file_name+'.txt')
+    with open(path,'w',encoding='utf-8') as f:
+        f.write(text)
 
 def download_file(url, save_dir):
-  os.makedirs(save_dir, exist_ok=True)
-  response = requests.get(url)
-  if response.status_code == 200:
-    save_path = os.path.join(save_dir, '')
-    with open(save_path, 'wb') as file:
-      file.write(response.content)
-      print(f"Downloaded: {save_path}")
-  else:
-    print(f"Failed to download: {url}")
+    os.makedirs(save_dir, exist_ok=True)
+    response = requests.get(url)
+    if response.status_code == 200:
+        save_path = os.path.join(save_dir, '')
+        with open(save_path, 'wb') as file:
+            file.write(response.content)
+            print(f"Downloaded: {save_path}")
+    else:
+        print(f"Failed to download: {url}")
 
-def get_passadas():
-  base_url="https://olimpiada.ic.unicamp.br"
-  html = urlopen(base_url+"/passadas/")
-  bs = BeautifulSoup(html, 'html.parser')
-  anchors = bs.select('.inner-page a')
-  hrefs=[]
-  for anchor in anchors:
-    hrefs.append(anchor.get('href'))
-  for href in hrefs:
-    html = urlopen(base_url+href)
+def get_passadas(): # AS QUESTOES NAO DISPONIVEIS NO "PRATIQUE" https://olimpiada.ic.unicamp.br/passadas/ NAO TEM TEXTO EM HTML, APENAS ARQUIVOS PDF PARA SEREM BAIXADOS POR download_file()
+    base_url="https://olimpiada.ic.unicamp.br"
+    html = urlopen(base_url+"/passadas/")
     bs = BeautifulSoup(html, 'html.parser')
-    anchors = []
-    for anchor in bs.select('a[href*="passadas"]'):
-      href_att = anchor.get('href')
-      if 'fase' in href_att and 'programacao' in href_att and 'merito' not in href_att:
-        anchors.append(href_att)
-    anchors
-
-def get_pratique():
-  base_url="https://olimpiada.ic.unicamp.br"
-  niveis = ['pj','p1','p2','pu']
-  for nivel in niveis:
-    print(nivel)
-    html = urlopen(base_url+"/pratique/"+nivel)
-    bs = BeautifulSoup(html, 'html.parser')
-    anchors = bs.select('li.atask a')
+    anchors = bs.select('.inner-page a')
     hrefs=[]
     for anchor in anchors:
-      hrefs.append(anchor.get('href'))
-    root = './OBI'
+        hrefs.append(anchor.get('href'))
     for href in hrefs:
-      print(href)
-      file_name,edicao,fase,text = get_text(base_url,href,nivel)
-      save_txt(root, file_name, edicao, fase, nivel, text)
+        html = urlopen(base_url+href)
+        bs = BeautifulSoup(html, 'html.parser')
+        anchors = []
+        for anchor in bs.select('a[href*="passadas"]'):
+            href_att = anchor.get('href')
+            if 'fase' in href_att and 'programacao' in href_att and 'merito' not in href_att:
+                anchors.append(href_att)
+    return anchors
+
+def get_pratique():
+    base_url="https://olimpiada.ic.unicamp.br"
+    niveis = ['pj','p1','p2','pu']
+    for nivel in niveis:
+        print(nivel)
+        html = urlopen(base_url+"/pratique/"+nivel)
+        bs = BeautifulSoup(html, 'html.parser')
+        anchors = bs.select('li.atask a')
+        hrefs=[]
+        for anchor in anchors:
+            hrefs.append(anchor.get('href'))
+            root = './OBI'
+        for href in hrefs:
+            print(href)
+            file_name,edicao,fase,text = get_text(base_url,href,nivel)
+            save_txt(root, file_name, edicao, fase, nivel, text)
 
 
 
