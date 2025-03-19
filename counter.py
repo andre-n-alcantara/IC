c5583b7b0b1aa3cafa6fa97200a25f3c3e3fdb82
andre.alcantara <andre.alcantara@sydle.com>
2025-03-19 00:00:31 -0300

    Makefile e compilados

--- counter.py
+++ counter.py
@@ -1,23 +1,15 @@
 import os
 import json
-# todos = []
-# with open('pratique.json', 'r', encoding='utf-8') as f:
-#     todos = json.load(f)
-# entries = [x[:-4] for x in os.listdir('./sol') if x.endswith('.cpp')]
-# todos2 = [] 
-# cntError = 0
-# cntAccept = 0
-# errors = []
-# for each in todos: 
-#     splitted = each.split('/')
-#     comp = splitted[3]+'-'+splitted[2]+'-'+splitted[4]+'-'+splitted[5]
-#     if(comp not in entries):
-#         cntError +=1
-#         errors.append(comp)
-#     else: 
-#         cntAccept +=1
-#     todos2.append(comp)
-# print(cntError)
-# print(cntAccept)
-# print(errors)
-# print(todos2[:10])
+directory = os.listdir('./pratique')
+for entry in directory:
+    if entry.endswith('.txt') and entry[:-4]+'.cpp' not in directory:
+        newname = entry[:-4]+'.cpp'
+        path = os.path.join('./sol',newname)
+        newpath = os.path.join('./pratique',newname)
+        with open(path, 'r') as f:
+            content = f.read()
+        with open(newpath,'w') as f2:
+            f2.write(content)
+        os.remove(path)
+        print(path)
+
